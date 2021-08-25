af24a2e22677a7afa6ce06689190c367e1a0b190
Matthew Harper <mattalanharper@gmail.com>
2021-08-25 19:51:42 -0400

    Initial Commit

---
+++ ExpanGe.py
@@ -0,0 +1,283 @@
+"""
+Author: Matthew Harper
+File: ExpanGe.py
+
+
+"""
+
+import sys
+import getopt
+from util import Node, DoublyLinkedList, Gene
+
+
+def identify_inversions(linked_list: DoublyLinkedList):
+    """
+    identify_inversions is a function designed to read through the lines in the .coord file and process all the data for later
+    use. It identifies where the reversals occur and will set the values for any values that are to be ignored. Uses a
+    recursive helper function
+    :param linked_list: The linked_list holding all the lines of the file
+    :return: None
+    """
+    head_node = linked_list.head
+    inversion_count = 0
+
+    def identify_inversions_helper(root: Node):
+        """
+        Recursive helper function for the indentify_inversions function.
+        Recusively loops through all nodes in the linked_list and records where inversions exist.
+        :param root: The current node
+        :return: None
+        """
+        if root.value.ignore is True:
+            identify_inversions_helper(root.next)
+        else:
+            ignore_flag = False
+            end_flag = False
+            current_start = root.value.start1
+            next_node = root.next
+
+            if next_node is None:
+                end_flag = True
+
+            while ignore_flag is False and end_flag is False:
+                if next_node.value.ignore is False:
+                    ignore_flag = True
+                else:
+                    next_node = next_node.next
+                    if next_node is None:
+                        end_flag = True
+
+            if end_flag is False:
+                next_start = next_node.value.start1
+
+                if next_start < current_start:
+                    next_node.value.reversed = True
+                    root.value.reversed = True
+                    root.value.inv_count = inversion_count
+                    next_node.value.inv_count = inversion_count
+
+                identify_inversions_helper(root.next)
+
+    identify_inversions_helper(head_node)
+
+
+def find_prev_valid(node: Node):
+    """
+    Helper function that will take in a node and find the closest, previous node that
+    is valid. This is used for calculating the distances between valid nodes.
+    :param node: A node containing data
+    :return: The previous, valid node
+    """
+    if node.prev is None:
+        return None
+
+    prev_node = node.prev
+
+    if prev_node.value.ignore is True:
+        prev_node = find_prev_valid(prev_node)
+
+    return prev_node
+
+
+def find_next_not_inverted(node: Node) -> Node:
+    """
+    Search function that will find the next node in the data that is not inverted.
+    :param node: Current node
+    :return: The not inverted node
+    """
+    if node is not None:
+        if node.value.reversed is True:
+            return find_next_not_inverted(node.next)
+        else:
+            return node
+    else:
+        return None
+
+
+def inverted_distance(node: Node):
+    """
+    Helper function to be called when there is an inversion in the data. This will calculate the distances
+    for all inverted nodes. Function is called in the calculate_distances function when an inversion is
+    detected.
+    :param node: The inverted node
+    :return: None
+    """
+    inversion_head = node
+    inversion_tail = None
+
+    def inverted_distance_helper(curr: Node, tail: Node):
+        """
+        Recursive helper function for the inverted_distance function
+        :param curr: Current Node
+        :param tail: The tail node (initially set to None, changed when the tail node is discovered)
+        :return: None
+        """
+        pass
+        """
+        TO DO: Write inversion distance helper function
+        Find a way to store the head and tail of inversion to calculate delta values for those nodes
+        """
+
+    inverted_distance_helper(inversion_head, inversion_tail)
+
+
+def calculate_distances(linked_list: DoublyLinkedList):
+    """
+    Function to calculate the distances between each gene in the coords file.
+    If a gene's previous gene is invalid, it will find the closest valid gene to calculate the
+    distance from.
+    Utilizes a recursive helper function.
+    :param linked_list: The data from the coords file
+    :return: None
+    """
+    def calculate_distances_helper(root: Node):
+        if root is None:
+            return None
+
+        if root.value.reversed is False:
+            prev_node = find_prev_valid(root)
+            if prev_node is not None:
+                root.value.delta_r = root.value.start1 - prev_node.value.end1
+                root.value.delta_q = root.value.start2 - prev_node.value.end2
+                root.value.delta_x = root.value.delta_r - root.value.delta_q
+            else:
+                root.value.delta_r = "--"
+                root.value.delta_q = "--"
+                root.value.delta_x = "--"
+
+            calculate_distances_helper(root.next)
+        else:
+            inverted_distance(root)
+            next_node = find_next_not_inverted(root)
+
+            if next_node is not None:
+                calculate_distances_helper(next_node.next)
+
+    head_node = linked_list.head
+
+    calculate_distances_helper(head_node)
+
+
+def display_help():
+    """
+    Function to display help information for the user.
+    This is displayed when there are no inputted arguments or when the user calls the
+    script with a "-h" flag as an argument.
+
+    :return: None
+    """
+    help_info = ""
+    print("Help Information for the ExpanGe program:")
+    print(help_info)
+
+
+def main(argv):
+    """
+    The main function for the script. Calls the helper functions to process the inputted data.
+    :param argv: The argument list
+    :return: None
+    """
+    file_name = ""
+    output_name = "ExpanGeOutput.coords"
+    sequence = DoublyLinkedList()
+    argument_list = argv
+    options = "hio:"
+    long_options = ["help", "input", "output"]
+
+    try:
+        flags, values = getopt.getopt(argument_list, options, long_options)
+
+        for current_flag, current_value in flags:
+            if current_flag in ("-h", "--help"):
+                display_help()
+            elif current_flag in ("-i", "--input"):
+                file_name = current_value
+            elif current_flag in ("-o", "--output"):
+                output_name = current_value
+
+    except getopt.error as err:
+        print(str(err))
+
+    if file_name == "":
+        display_help()
+        return None
+
+    # Opens the file and reads through each line.
+    # For loop will store the data on each line in a database for later usage
+    try:
+        data_file = open(file_name, 'r')
+    except FileNotFoundError:
+        print("Error: The inputted file was not found or could not be opened.")
+        print("EXITING PROGRAM")
+        return None
+
+    lines = data_file.readlines()
+    start_positions = set()
+    header = ""
+    count = 0
+
+    for line in lines:
+        if count > 5:
+            temp_gene = Gene()
+
+            # setting the values for the gene class
+            """
+            TO DO: Rewrite string splicing to make universal for all .coords files
+            """
+            temp_gene.start1 = line[:7].strip()
+            temp_gene.end1 = line[8:16].strip()
+            temp_gene.start2 = line[20:28].strip()
+            temp_gene.end2 = line[29:37].strip()
+            temp_gene.length1 = line[41:49].strip()
+            temp_gene.length2 = line[50:58].strip()
+            temp_gene.IDY = line[62:70].strip()
+            temp_gene.tag = line[74:96].strip()
+            temp_gene.scaffold = line[97:].strip()
+
+            # checking if a transposition has taken place, if there has been, ignore the line
+            if temp_gene.tag[12:] != temp_gene.scaffold:
+                temp_gene.ignore = True
+
+            # checking for multiples of genes
+            if temp_gene.start1 in start_positions:
+                temp_gene.ignore = True
+            else:
+                start_positions.add(temp_gene.start1)
+
+            if temp_gene.ignore is True:
+                temp_gene.delta_r = "--"
+                temp_gene.delta_x = "--"
+                temp_gene.delta_q = "--"
+                temp_gene.inv_count = "--"
+
+            sequence.push(temp_gene)
+
+        else:
+            header = header + line
+            count = count + 1
+
+    identify_inversions(sequence)
+    calculate_distances(sequence)
+
+    # Open the output file for writing, create it if it does not exist
+    output = open(output_name, "w+")
+
+    # Go through each line in the input file and write the data to the output file
+    # Add on the additional data calculated in this program at the end of each line
+    curr_node = sequence.head
+    output.write(header)
+
+    for line in lines:
+        output.write(line)
+        curr_node = sequence.head
+
+        if count > 5:
+            output.write(line)
+            '''
+            TO DO: Add columns at end of line for calculated data
+            '''
+            curr_node = curr_node.next
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ util.py
@@ -0,0 +1,193 @@
+"""
+Author: Matthew Harper
+File: util.py
+
+This file contains a series of utility functions and classes that are used in the ExpanGe program.
+The utilities are imported over to the ExpanGe.py file.
+"""
+
+from typing import TypeVar, List, Tuple
+import datetime
+
+T = TypeVar("T")
+Node = TypeVar("Node")
+
+
+class Node:
+    """
+    Implementation of a doubly linked list node.
+    Do not modify.
+    """
+    __slots__ = ["value", "next", "prev"]
+
+    def __init__(self, value: T, next: Node = None, prev: Node = None) -> None:
+        """
+        Construct a doubly linked list node.
+
+        :param value: value held by the Node.
+        :param next: reference to the next Node in the linked list.
+        :param prev: reference to the previous Node in the linked list.
+        :return: None.
+        """
+        self.next = next
+        self.prev = prev
+        self.value = value
+
+    def __repr__(self) -> str:
+        """
+        Represents the Node as a string.
+
+        :return: string representation of the Node.
+        """
+        return str(self.value)
+
+    def __str__(self) -> str:
+        """
+        Represents the Node as a string.
+
+        :return: string representation of the Node.
+        """
+        return str(self.value)
+
+
+class DoublyLinkedList:
+    __slots__ = ["head", "tail", "size"]
+
+    def __init__(self) -> None:
+        """
+        Construct an empty doubly linked list.
+
+        :return: None.
+        """
+        self.head = self.tail = None
+        self.size = 0
+
+    def __repr__(self) -> str:
+        """
+        Represent the DLL as a string.
+
+        :return: string representation of the DLL.
+        """
+        result = ""
+        node = self.head
+        while node is not None:
+            result += str(node)
+            if node.next is not None:
+                result += " <-> "
+            node = node.next
+        return result
+
+    def __str__(self) -> str:
+        """
+        Represent the DLL as a string.
+
+        :return: string representation of the DLL.
+        """
+        return repr(self)
+
+    def push(self, val: T, back: bool = True) -> None:
+        """
+        Create Node containing `val` and add to back (or front) of DLL. Increment size by one.
+
+        :param val: value to be added to the DLL.
+        :param back: if True, add Node containing value to back (tail-end) of DLL;
+            if False, add to front (head-end).
+        :return: None.
+        """
+
+        new_node = Node(val)
+        self.size = self.size + 1
+
+        if back:
+            new_node.prev = self.tail
+
+            if self.tail is None:
+                self.head = new_node
+                self.tail = new_node
+                new_node.next = None
+            else:
+                self.tail.next = new_node
+                new_node.next = None
+                self.tail = new_node
+
+        else:
+            new_node.next = self.head
+            new_node.prev = None
+
+            if self.head is None:
+                self.head = new_node
+                self.tail = new_node
+                new_node.prev = None
+
+            if self.head is not None:
+                self.head.prev = new_node
+                new_node.prev = None
+
+            self.head = new_node
+
+        pass
+
+    def pop(self, back: bool = True) -> None:
+        """
+        Remove Node from back (or front) of DLL. Decrement size by 1. If DLL is empty, do nothing.
+
+        :param back: if True, remove Node from (tail-end) of DLL;
+            if False, remove from front (head-end).
+        :return: None.
+        """
+
+        if self.size == 0:
+            pass
+        elif self.size == 1:
+            self.tail = None
+            self.head = None
+        elif back:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+
+        self.size = self.size - 1
+
+
+class Gene:
+    """
+    Gene class that represents each line in the inputted coords file
+    It contains the start and end points for both genes as well as the length
+    and the scaffold it is contained on.
+    """
+    def __init__(self):
+
+        # The data from each line in the coords file
+
+        # The start position of the reference gene
+        self.start1 = None
+        # The end position of the reference gene
+        self.end1 = None
+        # The start position of the query gene
+        self.start2 = None
+        # The end position of the query gene
+        self.end2 = None
+        # The length of the reference gene
+        self.length1 = None
+        # The length of the query gene
+        self.length2 = None
+        # TEMP
+        self.IDY = None
+        # TEMP
+        self.tag = None
+        # TEMP
+        self.scaffold = None
+        # Flag to determine if the gene is inverted
+        self.reversed = False
+        # Flag to determine whether the software will ignore this gene
+        self.ignore = False
+        # The distance between the reference nodes (start1 - end1)
+        self.delta_r = None
+        # The distance between the query nodes (start2 - end2)
+        self.delta_q = None
+        # The difference of delta_r and delta_q (delta_r - delta_q)
+        self.delta_x = None
+        # The number of inversion sections at this point
+        self.inv_count = None
